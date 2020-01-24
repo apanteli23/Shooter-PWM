8db3b3976a500ad211d2ceb8510faf1cf56b3d2d
apanteli23 <54606854+apanteli23@users.noreply.github.com>
2020-01-24 14:00:29 -0500

    Initial commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.Timer;
 
 
 
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.PWMSparkMax;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.ControlType;
@@ -27,12 +27,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * project.
  */
 public class Robot extends TimedRobot {
-  private CANSparkMax leftMotor = new CANSparkMax(1, MotorType.kBrushless);
-  private CANSparkMax rightMotor = new CANSparkMax(2, MotorType.kBrushless);
-  private CANEncoder leftEncoder = new CANEncoder(leftMotor);
-  private CANEncoder rightEncoder = new CANEncoder(rightMotor);
-  private CANPIDController m_pidController;
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, velocity, driverControl;
+  private PWMSparkMax leftMotor = new PWMSparkMax(1, MotorType.kBrushless);
+  private PWMSparkMax rightMotor = new PWMSparkMax(2, MotorType.kBrushless);
+  private double velocity;
   
   private final Timer m_timer = new Timer();
 
@@ -45,34 +42,7 @@ public class Robot extends TimedRobot {
     leftMotor.setInverted(true);
     leftMotor.follow(rightMotor);
     
-    m_pidController = rightMotor.getPIDController();
-
-    //pid coefficients
-    kP = 5e-5;
-    kI = 1e-6;
-    kD = 0;
-    kIz = 0;
-    kFF = 0;
-    kMaxOutput = 1;
-    kMinOutput = -1;
-    maxRPM = 5700;
-
-    m_pidController.setP(kP);
-    m_pidController.setI(kI);
-    m_pidController.setD(kD);
-    m_pidController.setIZone(kIz);
-    m_pidController.setFF(kFF);
-    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-    
-    SmartDashboard.putNumber("P Gain", kP);
-    SmartDashboard.putNumber("I Gain", kI);
-    SmartDashboard.putNumber("D Gain", kD);
-    SmartDashboard.putNumber("I Zone", kIz);
-    SmartDashboard.putNumber("Feed Forward", kFF);
-    SmartDashboard.putNumber("Max Output", kMaxOutput);
-    SmartDashboard.putNumber("Min Output", kMinOutput);
     SmartDashboard.putNumber("Velocity", velocity);
-    SmartDashboard.putNumber("Driver Control (1 or 0)", driverControl);
 
   }
 
@@ -92,39 +62,18 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-    double i = SmartDashboard.getNumber("I Gain", 0);
-    double d = SmartDashboard.getNumber("D Gain", 0);
-    double iz = SmartDashboard.getNumber("I Zone", 0);
-    double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    double max = SmartDashboard.getNumber("Max Output", 0);
-    double min = SmartDashboard.getNumber("Min Output", 0);
-
-
-
+    
     //get velocity from SmartDashboard
-    double velocity = SmartDashboard.getNumber("Velocity", 0);
+    double m_velocity = SmartDashboard.getNumber("Velocity", 0);
 
-
-    // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
+    // if velocity coefficients on SmartDashboard have changed, write new values to controller
+    if(m_velocity != velocity) { 
+      m_velocity = velocity;
     }
     
-    
-    double speed = velocity*maxRPM;
-    m_pidController.setReference(speed, ControlType.kVelocity);
+    double speed = velocity;
       
     SmartDashboard.putNumber("CurretVelocity", speed);
-    SmartDashboard.putNumber("ProcessVariable", rightEncoder.getVelocity());
-    SmartDashboard.putNumber("ProcessVariable", leftEncoder.getVelocity());
   }
 
   @Override
